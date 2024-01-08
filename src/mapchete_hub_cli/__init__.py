cdfd934497697540081b695661f1da46c0cce57e
Petr Ševčík <petr.sevcik@eox.at>
2024-01-08 15:22:33 +0100

    dont be stupid petr!

--- src/mapchete_hub_cli/__init__.py
+++ src/mapchete_hub_cli/__init__.py
@@ -2,6 +2,8 @@ from mapchete_hub_cli._client import (
     COMMANDS,
     DEFAULT_TIMEOUT,
     JOB_STATUSES,
+    MHUB_CLI_ZONES_WAIT_TILES_COUNT,
+    MHUB_CLI_ZONES_WAIT_TIME_SECONDS,
     Client,
     load_mapchete_config,
 )
@@ -11,6 +13,8 @@ __all__ = [
     "COMMANDS",
     "DEFAULT_TIMEOUT",
     "JOB_STATUSES",
+    "MHUB_CLI_ZONES_WAIT_TILES_COUNT",
+    "MHUB_CLI_ZONES_WAIT_TIME_SECONDS",
     "load_mapchete_config",
 ]
 __version__ = "2023.12.1"
