3a839b3d50fb507cf98a6233fef8c735ce9ca521
Joachim Ungar <joachim.ungar@gmail.com>
2024-02-06 15:42:48 +0100

    move confi gloader into new parser submodule

--- src/mapchete_hub_cli/client.py
+++ src/mapchete_hub_cli/client.py
@@ -10,14 +10,12 @@ import datetime
 import json
 import logging
 import os
-import py_compile
 import time
 from collections import OrderedDict
 from dataclasses import dataclass
 from json.decoder import JSONDecodeError
-from typing import Any, Generator, Iterator, List, Optional, Tuple, Union
+from typing import Generator, Iterator, List, Optional, Tuple, Union
 
-import oyaml as yaml
 import requests
 from requests.exceptions import HTTPError
 
@@ -28,6 +26,7 @@ from mapchete_hub_cli.exceptions import (
     JobNotFound,
     JobRejected,
 )
+from mapchete_hub_cli.parser import load_mapchete_config
 from mapchete_hub_cli.time import date_to_str, passed_time_to_timestamp, str_to_date
 
 logger = logging.getLogger(__name__)
@@ -506,77 +505,6 @@ class Client:
 
     def __repr__(self):  # pragma: no cover
         return f"Client(host={self.host}, user={self._user}, password={self._password})"
-
-
-def load_mapchete_config(
-    mapchete_config: Union[dict, str], basedir: Optional[str] = None
-) -> dict:
-    """
-    Return preprocessed mapchete config provided as dict or file.
-
-    This function reads a mapchete config into an OrderedDict which keeps the item order
-    statusd in the .mapchete file.
-    If the configuration is passed on via a .mapchete file and if a process file path
-    instead of a process module path was given, it will also check the syntax and replace
-    the process item with the python code as string.
-
-    Parameters
-    ----------
-    mapchete_config : str or dict
-        A valid mapchete configuration either as path or dictionary.
-
-    Returns
-    -------
-    OrderedDict
-        Preprocessed mapchete configuration.
-    """
-    basedir = basedir or os.getcwd()
-    if isinstance(mapchete_config, dict):
-        conf = cleanup_dict(mapchete_config)
-    elif isinstance(mapchete_config, str):
-        basedir = os.path.dirname(mapchete_config)
-        conf = cleanup_dict(yaml.safe_load(open(mapchete_config, "r").read()))
-    else:  # pragma: no cover
-        raise TypeError(
-            "mapchete config must either be a path to an existing file or a dict"
-        )
-
-    process = conf.get("process")
-
-    # handle process
-    if not process:  # pragma: no cover
-        raise KeyError("no or empty process in configuration")
-
-    if isinstance(process, str):
-        # local python file
-        if process.endswith(".py"):
-            custom_process_path = os.path.join(basedir, process)
-            # check syntax
-            py_compile.compile(custom_process_path, doraise=True)
-            # assert file is not empty
-            process_code = open(custom_process_path).read()
-            if not process_code:  # pragma: no cover
-                raise ValueError("process file is empty")
-            conf.update(process=process_code.splitlines())
-
-    return conf
-
-
-def cleanup_dict(value: dict) -> OrderedDict:
-    """Convert datetime objects in dictionary to strings."""
-    return OrderedDict([(k, cleanup_datetime(v)) for k, v in value.items()])
-
-
-def cleanup_datetime(value: Any) -> Any:
-    """Convert datetime objects in dictionary to strings."""
-    if isinstance(value, dict):
-        return OrderedDict([(k, cleanup_datetime(v)) for k, v in value.items()])
-    elif isinstance(value, (list, tuple)):
-        return [cleanup_datetime(ii) for ii in value]
-    elif isinstance(value, datetime.date):
-        return str(value)
-    else:
-        return value
 
 
 def to_statuses(status: Optional[Union[List[Status], Status]] = None) -> List[Status]:
